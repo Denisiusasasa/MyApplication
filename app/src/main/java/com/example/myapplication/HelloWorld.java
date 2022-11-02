2ce68830b20a11b358d8f365311e91ac5f1634e7
Денис Кузьменко <denissius16@gmail.com>
2022-11-02 20:22:32 +0300

    new

--- app/src/main/java/com/example/myapplication/HelloWorld.java
+++ app/src/main/java/com/example/myapplication/HelloWorld.java
@@ -2,6 +2,6 @@ package com.example.myapplication;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.print("HelloWorld!");
+        System.out.print("HelloWorld! 10");
     }
 }
