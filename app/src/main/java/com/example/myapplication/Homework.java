2a5d527f95e8cf855b3e650185c8f142f4c63db3
Денис Кузьменко <denissius16@gmail.com>
2022-11-13 16:23:00 +0300

    task2

--- app/src/main/java/com/example/myapplication/Homework.java
+++ app/src/main/java/com/example/myapplication/Homework.java
@@ -1,4 +1,27 @@
 package com.example.myapplication;
-
+import java.util.Scanner;
 public class Homework {
+    public static void main(String[] args) {
+        task2();}
+
+    public static void task2() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Первое число: ");
+        int FirstNum = scanner.nextInt();
+        int first = FirstNum > 0 ? FirstNum : -FirstNum;
+        System.out.print("Второе число: ");
+        int SecondNum = scanner.nextInt();
+        int second = SecondNum > 0 ? SecondNum : -SecondNum;
+        System.out.print("Третье число: ");
+        int ThirdNum = scanner.nextInt();
+        int third = ThirdNum > 0 ? ThirdNum : -ThirdNum;
+
+        if (first < second && first < third) {
+            System.out.println(first + " меньшее число ");
+        } else if (second < first && second < third) {
+            System.out.println(second + " меньшее число ");
+        } else if  (third < first && third < second){
+            System.out.println(third + " меньшее число ");
+        }
+    }
 }
