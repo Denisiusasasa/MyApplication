9426bdbd71572ab409d7008c36bddfd927f81a62
Денис Кузьменко <denissius16@gmail.com>
2022-11-13 16:54:15 +0300

    task3

--- app/src/main/java/com/example/myapplication/Homework.java
+++ app/src/main/java/com/example/myapplication/Homework.java
@@ -1,4 +1,38 @@
 package com.example.myapplication;
-
+import java.util.Scanner;
 public class Homework {
+    public static void main(String[] args) {
+    task3();
+    }
+
+    static void task3() {
+        System.out.println("Введите любое число в промежутке от 1 до 7");
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        switch (a) {
+            case 1:
+                System.out.println("Понедельник");
+                break;
+            case 2:
+                System.out.println("Вторник");
+                break;
+            case 3:
+                System.out.println("Среда");
+                break;
+            case 4:
+                System.out.println("Четверг");
+                break;
+            case 5:
+                System.out.println("Пятница");
+                break;
+            case 6:
+                System.out.println("Суббота");
+                break;
+            case 7:
+                System.out.println("Воскресенье");
+                break;
+            default:
+                System.out.println("Неизвестный день недели");
+        }
+    }
 }
