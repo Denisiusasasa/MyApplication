fc6c4e8296ab988c0fcf5b10d5f2b4a8091a173a
Денис Кузьменко <denissius16@gmail.com>
2022-11-13 14:46:04 +0300

    task 1

--- app/src/main/java/com/example/myapplication/Homework.java
+++ app/src/main/java/com/example/myapplication/Homework.java
@@ -1,24 +1,19 @@
 package com.example.myapplication;
-
 import java.util.Scanner;
-
 public class Homework {
 
         public static void main(String[] args) {
             task1();
         }
-
         public static void task1() {
             Scanner scanner = new Scanner(System.in);
-
-            System.out.print("Enter a number: ");
+            System.out.print("Введите число: ");
             int input = scanner.nextInt();
             if (input % 2 == 0) {
-                System.out.println("This number is even");
+                System.out.println("Это число четное");
             } else {
-                System.out.println("This number is odd");
+                System.out.println("Это число нечетное");
             }
         }
-
     }
 
