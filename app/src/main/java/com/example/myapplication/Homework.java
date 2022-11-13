7ee6fbaf8ec068d34f210bb48b146c13809253e6
Denisiusasasa <100040907+Denisiusasasa@users.noreply.github.com>
2022-11-13 17:31:32 +0300

    task4

--- app/src/main/java/com/example/myapplication/Homework.java
+++ app/src/main/java/com/example/myapplication/Homework.java
@@ -1,4 +1,14 @@
 package com.example.myapplication;
 
 public class Homework {
+    public static void main(String[] args){
+    task4();}
+
+    public static void task4() {
+        for (int i = 0; i <= 100; i += 5) {
+            if (i != 0) {
+                System.out.println(i);
+            }
+        }
+    }
 }
