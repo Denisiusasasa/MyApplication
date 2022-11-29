80062d3eb2fd04dfb1998b6795f26f304d66eb13
Denisiusasasa <100040907+Denisiusasasa@users.noreply.github.com>
2022-11-29 14:58:22 +0300

    Homework

--- app/src/main/java/com/example/myapplication/Lesson8/Homework/FakeWorkplace.java
+++ app/src/main/java/com/example/myapplication/Lesson8/Homework/FakeWorkplace.java
@@ -1,5 +1,5 @@
 package com.example.myapplication.Lesson8.Homework;
 
  abstract public class FakeWorkplace {
-  Workplace workplace = new Workplace("HP", "Logitech", "Logitech", "Samsung");
+  Workplace workplace = new Workplace("Macbook");
 }
