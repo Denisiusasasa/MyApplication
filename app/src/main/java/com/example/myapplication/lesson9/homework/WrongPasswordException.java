c85505d68f1f2f39c311466c89fa741d2e73630f
Denisiusasasa <100040907+Denisiusasasa@users.noreply.github.com>
2022-12-04 19:16:19 +0300

    Homework

---
+++ app/src/main/java/com/example/myapplication/lesson9/homework/WrongPasswordException.java
@@ -0,0 +1,11 @@
+package com.example.myapplication.lesson9.homework;
+
+public class WrongPasswordException extends Exception {
+    public WrongPasswordException(){
+    }
+
+    public WrongPasswordException(String message) {
+        super(message);
+    }
+}
+
